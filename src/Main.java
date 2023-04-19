1e55b74de5b141d7bb13d3546bfe1e2d291d95f7
almiinho <almazazamatov863@gmail.com>
2023-04-19 23:31:59 +0600

    c

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         MyArrayList <Integer>alm = new MyArrayList();
-        System.out.println(alm.size());
+        System.out.println(alm.size());//checking the size of array
         alm.add(1);
         alm.add(2);
         alm.add(3);
         alm.add(4);
         alm.add(5);
-        System.out.println(alm.size());
+        System.out.println(alm.size());//checking the size of array
         System.out.println(alm.get(3));
         System.out.println(alm.remove(2));
-        System.out.println(alm.size());
+        System.out.println(alm.size());//checking the size of array
         System.out.println(alm.indexOf(1));
         System.out.println(alm.lastIndexOf(1));
         System.out.println("End of  using ArrayList");
 
         MyLinkedList<String> ll = new MyLinkedList<>();
+        System.out.println(ll.size());//checking the size of array
         ll.add("Real Madrid");
         ll.add("Barcelona");
         ll.add("PSG");
@@ -23,7 +24,8 @@ public class Main {
         ll.add("Irtysh");
         System.out.println(ll.get(0));
         System.out.println(ll.remove(3));
-        System.out.println(ll.size());
+        System.out.println(ll.size());//checking the size of array
+        System.out.println("End of  using LinkedList");
     }
 }
 
